bcb6ab5d0aa53b4ce814c9ffdd39f0860cd5da24
Batun İdikurt <82970523+batiii14@users.noreply.github.com>
2021-04-29 21:56:27 +0300

    Update TeamManager.java

--- firstAssignment/src/firstAssignment/TeamManager.java
+++ firstAssignment/src/firstAssignment/TeamManager.java
@@ -2,7 +2,7 @@ package firstAssignment;
 
 public class TeamManager {
 		public void showMeinfo(Team team,Category category) {
-			System.out.println("Now can see all the details of "+team.name+"\nCity "+team.city+"\nFans "+team.nicknamesOfFans+"\nLeague "+category.name+"\n");
+			System.out.println("Now you can see all the details of "+team.name+"\nCity "+team.city+"\nFans "+team.nicknamesOfFans+"\nLeague "+category.name+"\n");
 			
 		}
 
